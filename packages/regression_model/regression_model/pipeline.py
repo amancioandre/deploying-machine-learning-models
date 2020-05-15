be0be34289ac8e1597af853b162d124a12337589
André Moraes <amancioandre@gmail.com>
2020-05-15 15:35:40 -0300

    :construction: Section 6.3 - Connecting the Pipeline

--- packages/regression_model/regression_model/pipeline.py
+++ packages/regression_model/regression_model/pipeline.py
@@ -1,25 +1,45 @@
 from sklearn.pipeline import Pipeline
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import MinMaxScaler
 
-import preprocessor as pp
 
-CATEGORICAL_VARS = [
-    'MSZoning',
-    'Neighborhood',
-    'RoofStyle',
-    'MasVnrType',
-    'BsmtQual',
-    'BsmtExposure',
-    'HeatingQC',
-    'CentralAir',
-    'KitchenQual',
-    'FireplaceQu',
-    'GarageType',
-    'GarageFinish',
-    'PavedDrive'
-]
+from regression_model.processing import preprocessors as pp
+from regression_model.config import config
 
-PIPELINE_NAME = 'lasso_regression'
-
-price_pipe = Pipeline([
-    ('categorigal_imputer', pp.CategoricalImputer(variables=CATEGORICAL_VARS))
-])
+price_pipe = Pipeline(
+    [
+        (
+            "categorical_imputer",
+            pp.CategoricalImputer(
+                variables=config.CATEGORICAL_VARS_WITH_NA),
+        ),
+        (
+            "numerical_inputer",
+            pp.NumericalImputer(variables=config.NUMERICAL_VARS_WITH_NA),
+        ),
+        (
+            "temporal_variable",
+            pp.TemporalVariableEstimator(
+                variables=config.TEMPORAL_VARS, reference_variable=config.DROP_FEATURES
+            ),
+        ),
+        (
+            "rare_label_encoder",
+            pp.RareLabelCategoricalEncoder(
+                tol=0.01, variables=config.CATEGORICAL_VARS),
+        ),
+        (
+            "categorical_encoder",
+            pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS),
+        ),
+        ("log_transformer", pp.LogTransformer(
+            variables=config.NUMERICAL_LOG_VARS)),
+        (
+            "drop_features",
+            pp.DropUnecessaryFeatures(
+                variables_to_drop=config.DROP_FEATURES),
+        ),
+        ("scaler", MinMaxScaler()),
+        ("Linear_model", Lasso(alpha=0.005, random_state=0)),
+    ]
+)
